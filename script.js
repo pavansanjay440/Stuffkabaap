9154962a57a2ea06c5bab9231a99500921f2868d
Mr.Pavan <109209957+pavansanjay440@users.noreply.github.com>
2023-05-06 13:09:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let ham = document.querySelector("input");
+let sidebar = document.querySelector(".sidebar");
+let links = document.querySelectorAll("links");
+console.log(document.querySelector("input")); 
+console.log(ham);
